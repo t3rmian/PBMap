f371af90e45f43c8586f654023c43a110fc0a95e
Damian Terlecki <t3r1jj@gmail.com>
2019-03-24 17:21:25 +0100

    Use transfer.sh for uploading logs

--- ci/upload_screenshots.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-tar -czf `date "+%Y.%m.%dT%H.%M.%S_ss.tar.gz"` $2
-curl -X POST https://content.dropboxapi.com/2/files/upload \
-  --header "Authorization: Bearer $1" \
-  --header "Dropbox-API-Arg: {\"path\": \"/ss/`ls -A1 *_ss.tar.gz`\"}" \
-  --header "Content-Type: application/octet-stream" \
-  --data-binary @`ls -A1 *_ss.tar.gz`
