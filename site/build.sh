354a88c900f68b894608ba81250a28ee3c9c9712
t3rmian <t3rmian@gmail.com>
2019-12-02 20:16:02 +0200

    Prepare site index structure

--- site/build.sh
+++ site/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+rm -rf public
 mkdir public
 
 echo "=== Generating hash ==="
-./hash.sh ../app/src/main/assets/data/* ./*.sh ./*.html > public/.hash
+./hash.sh ../app/src/main/assets/data/* ./*.sh ./*.html ./*.png > public/.hash
 cat public/.hash
 echo "=== Downloading hash from remote ==="
 curl https://pbmap.termian.dev/.hash > .hash
@@ -16,7 +17,7 @@ then
     exit 0
 fi
 echo "=== The hashes are different, continuing the build ==="
-
+cp logo.png public/logo.png
 for file in ../app/src/main/assets/data/*
 do
 	echo "=== Processing $file ==="
@@ -40,9 +41,19 @@ echo "Generating main index..."
 cp index.html public/index.html
 for file in public/*.html public/*/*.html public/*/*/*.html public/*/*/*/*.html;
 do
-    path=$(echo "$file" | cut -d/ -f2-)
-    link="<a href=\"$path\">$path</a></br>"
+    path=$(echo "$file" | cut -d/ -f2,3,4)
+	place=$(echo "$path" | cut -d/ -f3)@
+	space=$(echo "$path" | cut -d/ -f2)
+	if [[ "$space" == "index.html" ]]
+    then
+        continue
+    fi
+	if [[ "$place" == "index.html@" ]]
+    then
+        place=""
+    fi
+    link="<li class=\"hide-default\"><a href=\"$path\">$place$space</a></li>"
     sed -i "s|%APPEND%|$link&|g" public/index.html
 done
-sed -i "s|%APPEND%|</br>|g" public/index.html
+sed -i "s|%APPEND%||g" public/index.html
 echo "Finished generating main index"
