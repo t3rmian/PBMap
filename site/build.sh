026946ab5b4f3160dc7e7bab8c6bc61b88ca3aeb
t3rmian <t3rmian@gmail.com>
2019-12-03 19:38:03 +0100

    Implement simple site i18n

--- site/build.sh
+++ site/build.sh
@@ -4,7 +4,7 @@ rm -rf public
 mkdir public
 
 echo "=== Generating hash ==="
-./hash.sh ../app/src/main/assets/data/* ./*.sh ./*.html ./*.png > public/.hash
+./hash.sh ../app/src/main/assets/data/* ./*.sh ./*.html ./*.png ../app/src/main/res/values*/*data.xml > public/.hash
 cat public/.hash
 echo "=== Downloading hash from remote ==="
 curl https://pbmap.termian.dev/.hash > .hash
@@ -27,9 +27,9 @@ do
 done
 wait
 
-echo "Generating main index..."
+echo "=== Generating main index... ==="
 cp index.html public/index.html
-for file in public/*.html public/*/*.html public/*/*/*.html public/*/*/*/*.html;
+for file in public/*.html public/*/*.html public/*/*/*.html public/*/*/*/*.html
 do
     path=$(echo "$file" | cut -d/ -f2,3,4)
 	place=$(echo "$path" | cut -d/ -f3)@
@@ -42,7 +42,33 @@ do
     then
         place=""
     fi
-    link="$link<li class=\"hide-default\"><a href=\"/$path\">$place$space</a></li>"
+    link="$link<li class=\"hide-default\"><a href=\"/$path\" data-id=\"$place$space\"></a></li>"
 done
-sed -i "s|%APPEND%|$link&|g" public/index.html
-echo "Finished generating main index"
+sed -i "s|%APPEND%|$link|g" public/index.html
+echo "=== Finished generating main index ==="
+
+echo "=== Preparing i18n files... ==="
+for file in ../app/src/main/res/values*/*data.xml
+do
+    echo "=== Processing $file ==="
+    values_name=$(echo "$file" | cut -d/ -f6)
+    lang=$(echo "$values_name" | cut -d- -f2)
+    region=$(echo "$values_name" | cut -d- -f3 | cut -c2- | awk '{print tolower($0)}')
+
+    if [[ "$region" == "" ]]
+    then
+        hreflang=$lang
+    else
+        hreflang="$lang-$region"
+    fi
+
+    if [[ "$lang" == "values" ]]
+    then
+        sed '/<string name="address_/,/<\/string>/d' $file > public/i18n.xml
+    else
+        mkdir -p public/$hreflang
+        cp public/index.html public/$hreflang/index.html
+        sed '/<string name="address_/,/<\/string>/d' $file > public/$hreflang/i18n.xml
+    fi
+done
+echo "=== Finished preparing i18n files ==="
